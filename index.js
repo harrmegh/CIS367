49312ac71010b92bf177b04be5a5686c627e8eca
Meghan Harris <71603666+harrmegh@users.noreply.github.com>
2023-01-26 17:45:01 -0500

    animating square

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { drawScene } from "./draw-scene.js";
 
 main();
 
+let squareRotation = 0.0;
+let deltaTime = 0;
 //
 // start here
 //
@@ -81,7 +83,20 @@ function main() {
   const buffers = initBuffers(gl);
 
   // Draw the scene
-  drawScene(gl, programInfo, buffers);
+  let then = 0;
+
+  // Draw the scene repeatedly
+  function render(now) {
+    now *= 0.001; // convert to seconds
+    deltaTime = now - then;
+    then = now;
+
+    drawScene(gl, programInfo, buffers, squareRotation);
+    squareRotation += deltaTime;
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
 
 //
