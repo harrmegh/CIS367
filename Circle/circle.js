a66a086c75223a9919b584311c9a3ffb166c8d87
Meghan Harris <71603666+harrmegh@users.noreply.github.com>
2023-01-26 15:43:20 -0500

    triangle to index.js as proof of concept and added not working circle

--- Circle/circle.js
+++ Circle/circle.js
@@ -1,8 +1,11 @@
-// index.js
-// Author Meghan Harris
+// Erik Fredericks with a few notations by Meghan Harris
+// Graphics object
 var gl;
 var points;
+var vertices;
 
+// Once browser loaded, run this js code, aka main()
+// Purpose is to set up the WebGL context and start rendering content
 window.onload = function init() {
   // Setup our canvas and WebGL
   // Acts as our reference to canvas
@@ -15,11 +18,21 @@ window.onload = function init() {
     alert("WebGL unavailable");
   }
   // Triangle vertices
-  var vertices = [vec2(-1, -1), vec2(0, 1), vec2(1, -1)];
+  vertices = [vec2(0, 0)];
+
+  let step = Math.PI / 256;
+
+  for (let t = 0; t < Math.PI * 2.0 + step; t += step) {
+    let x = 0.75 * Math.cos(t);
+    let y = 0.75 * Math.sin(t);
+
+    // something
+    vertices.push(vec2(x, y));
+  }
 
   // configure WebGL, canvas in middle (0, 0)
   gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // load and initialize shaders
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
@@ -34,12 +47,12 @@ window.onload = function init() {
   var vPosition = gl.getAttribLocation(program, "vPosition");
   gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(vPosition);
-
   render();
 };
 
 // Package it up and ship it off
 function render() {
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  console.log("Vertices: ", vertices);
+  gl.drawArrays(gl.TRIANGLES_FAN, 0, vertices.length);
 }
