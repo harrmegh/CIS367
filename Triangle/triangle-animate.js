ffd5200b59a292ed050ba07289a471103e4b0815
Meghan Harris <71603666+harrmegh@users.noreply.github.com>
2023-02-07 18:56:24 -0500

    rearranging some logic

--- Triangle/triangle-animate.js
+++ Triangle/triangle-animate.js
@@ -1,25 +1,34 @@
 // Erik Fredericks with a few notations by Meghan Harris
 // Graphics object
 var gl;
+var thetaLoc;
+var theta = 0.0;
 
 var colors = [vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.5), vec3(0.0, 0.0, 0.5)];
 
 // Once browser loaded, run this js code, aka main()
 // Purpose is to set up the WebGL context and start rendering content
+// Onload tells us everything is ready to go: compiled, loaded, etc.
 window.onload = function init() {
   // Setup our canvas and WebGL
+  // window.addEventListener(
+  //   "keydown",
+  //   function (e) {
+  //     $("#key").html("K")
+  //   }
+  // )
   // Acts as our reference to canvas
   var canvas = document.getElementById("gl-canvas");
 
   // Get a context for the canvas
   // If not null, gl is our reference to the context
   gl = WebGLUtils.setupWebGL(canvas);
+
   if (!gl) {
     alert("WebGL unavailable");
   }
   // Triangle vertices
-  var vertices = [vec2(-1, -1), vec2(0, 1), vec2(1, -1)];
-  var thetaLoc = gl.getUniformLocation(program, "theta");
+  var vertices = [vec2(-0.25, -0.25), vec2(0, 0.25), vec2(0.25, -0.25)];
 
   // configure WebGL, canvas in middle (0, 0)
   gl.viewport(0, 0, canvas.width, canvas.height);
@@ -27,6 +36,7 @@ window.onload = function init() {
 
   // load and initialize shaders
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
+
   gl.useProgram(program);
 
   // load data into GPU
@@ -47,17 +57,20 @@ window.onload = function init() {
   gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(vColor);
 
-  //   requestAnimationFrame(render);
+  thetaLoc = gl.getUniformLocation(program, "theta");
+
   render();
 };
 
 // Package it up and ship it off
 function render() {
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  // gl.clear(gl.COLOR_BUFFER_BIT);
 
   theta += 0.1;
   gl.uniform1f(thetaLoc, theta);
 
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  requestAnimationFrame(render);
+  requestAnimFrame(render);
+  // setTimeout(render, 1000);
+  // render();
 }
